57cf3a84f0ba5ac7aa115ec5800b5e734daf580f
Antonio Caravello <acaravello@xeniaprogetti.it>
2021-10-11 10:45:35 +0200

    Deploying fe and be

--- be/app.js
+++ be/app.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const port = process.env.port || 1337;
+const port = process.env.PORT || 8080;
 const app = express();
 const productsApi = require('./modules/productsAPI');
 const middleware = require('./modules/middleware');
--- be/modules/middleware.js
+++ be/modules/middleware.js
@@ -1,8 +1,7 @@
 
 
 const middlewareCors = (req, res, next) => {
-    const origin = req.headers.origin;
-    res.setHeader('Access-Control-Allow-Origin', origin || '*');
+    res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
